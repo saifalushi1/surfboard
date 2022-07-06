5343e91ed9089697d82b8c0bebda5af992413083
SaifSushi <alushisaif@gmail.com>
2022-07-06 00:02:28 -0500

    added calendar and time for meetings

--- src/components/CreateMeeting.tsx
+++ src/components/CreateMeeting.tsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
+import setHours from 'date-fns/setHours';
+import setMinutes from 'date-fns/setMinutes';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
 const CreateMeeting = (): JSX.Element => {
   const [startDate, setStartDate] = useState(new Date());
-  return <DatePicker selected={startDate} onChange={(date: Date) => setStartDate(date)} />;
+
+  console.log(startDate);
+
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date: Date) => setStartDate(date)}
+      inline
+      showTimeSelect
+      minTime={setHours(setMinutes(new Date(), 0), 9)}
+      maxTime={setHours(setMinutes(new Date(), 30), 17)}
+      dateFormat="MMMM d, yyyy h:mm aa"
+    />
+  );
 };
 
 export default CreateMeeting;
